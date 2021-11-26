1e3070ab0fb5ad0ed0dbd4dfa55d35c6b18053ac
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-11-26 12:58:01 +0600

    Results
    results.html
    - codebase is formatted with prettier and autopep8 linter

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,4 @@
+from .models import Question, Choice
 from django.contrib import admin
 
 admin.site.site_header = "Pollster Admin"
@@ -6,18 +7,20 @@ admin.site.index_title = "Welcome to the Pollster admin area"
 
 # Register your models here.
 
-from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
-  model = Choice
-  extra = 3
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
-  fieldsets = [
-                (None, {'fields': ['question_text']}),
-                ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-              ]
-  inlines = [ChoiceInline]
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': [
+            'pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
 
 # admin.site.register(Question)
 # admin.site.register(Choice)
